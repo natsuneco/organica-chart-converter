b7f3c5171b9a5c27a0fb7e5fe560db33de4955ad
Natsuneco <135076690+Natsuneco@users.noreply.github.com>
2025-10-02 19:01:22 +0900

    スペルミスの修正

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 # main.pyをビルド対象にし、baseを設定する
-executables = [Executable("main.py", base=base, icon="chart_convert_icon.ico")]
+executables = [Executable("main.py", base=base, icon="chart_converter_icon.ico")]
 
 # --- セットアップ情報の記述 ---
 setup(
